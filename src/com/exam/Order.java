34a39d6dc72bdfa4fdc62e0ec63e5ede344fa397
wxuanzhi <wxuanzhi@gmail.com>
2018-12-11 12:00:25 +0800

    in class unfinished job

---
+++ src/com/exam/Order.java
@@ -0,0 +1,8 @@
+package com.exam;
+
+import java.util.ArrayList;
+
+public class Order {
+	ArrayList bag = new ArrayList();
+	
+}
---
+++ src/com/exam/Ticket.java
@@ -0,0 +1,16 @@
+package com.exam;
+
+public class Ticket {
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+	
+
+	public Ticket(String origin, String destination, int price, int quantity){
+	this.origin = origin;
+	this.destination = destination;
+	this.price = price;
+	this.quantity = quantity;
+	}
+	}
